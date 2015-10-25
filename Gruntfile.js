cd610453e45c06cc8864fcd69d747c68cc3f2cc6
Daniel Ritz <daniel.ritz@gmx.ch>
2015-10-25 19:39:28 +0100

    SCM stuff must be in scm.css, not part of application.css
    Redmine loads a separate scm.css after application.css. Instead of
    adding adding the !important markers everywhere, create a scm.css in
    the theme. This is automatically loaded. To make it work, copy all
    definitions from Redmine's scm.css, sassified.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,32 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			options: {
-				sourceMap: true,
-				precision: 5,
-				outputStyle: 'expanded',
-				noCache: false,
-				spawn: false
-			},
-			dist: {
-				files: {
-					'stylesheets/application.css': 'sass/application.scss'
-				}
-			}
-		},
+        sass: {
+            options: {
+                sourceMap: true,
+                precision: 5,
+                outputStyle: 'expanded',
+                noCache: false,
+                spawn: false
+            },
+            dist: {
+                files: {
+                    'stylesheets/application.css': 'sass/application.scss',
+                    'stylesheets/scm.css': 'sass/scm.scss'
+                }
+            }
+        },
         cssmin: {
             minify: {
-                files:[{
+                files: [
+                  {
                     src: 'stylesheets/application.css',
                     dest: 'stylesheets/application.min.css'
-                }],
+                  }, {
+                    src: 'stylesheets/scm.css',
+                    dest: 'stylesheets/scm.min.css'
+                  }
+                ],
                 options: {
                     report:'min',
                     keepSpecialComments: 0
@@ -33,23 +39,23 @@ module.exports = function(grunt) {
                 tasks: ['sass']
             }
         },
-		compress: {
-			main: {
-				options: {
-					archive: 'minelab-ritz.tar.gz',
-					pretty: true,
-				},
-				expand: true,
-				src: [
-					'fonts/**',
-					'images/**',
-					'javascripts/**',
-					'stylesheets/**',
-					'README.md'
-				],
-				dest: '/minelab-ritz'
-			}
-		}
+        compress: {
+            main: {
+                options: {
+                    archive: 'minelab-ritz.tar.gz',
+                    pretty: true,
+                },
+                expand: true,
+                src: [
+                    'fonts/**',
+                    'images/**',
+                    'javascripts/**',
+                    'stylesheets/**',
+                    'README.md'
+                ],
+                dest: '/minelab-ritz'
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-sass');
